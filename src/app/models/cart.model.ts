a16bd04f1a6582a7c49adc74832639e1e491718d
Mikhail Vasiliev <mikhail@kenmoredesign.com>
2023-09-06 20:21:57 +0300

    fix cart

--- src/app/models/cart.model.ts
+++ src/app/models/cart.model.ts
@@ -1,11 +1,11 @@
 export interface CartItem {
-  product: string
-  name: string
-  price: number
-  quantity: number
-  id: number
+  product: string;
+  name: string;
+  price: number;
+  quantity: number;
+  id: number;
 }
 
-export interface Cart{
-  items: Array<CartItem>
+export interface Cart {
+  items: CartItem[];
 }
--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,33 +1,40 @@
-import {Component, OnInit} from '@angular/core';
-import {Cart, CartItem} from "../../models/cart.model";
+import { Component, OnInit } from '@angular/core';
+import { Cart, CartItem } from '../../models/cart.model';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
-export class CartComponent implements OnInit{
-cart: Cart = {items: [{
-    product: 'https://via.placeholder.com/150',
-    name: 'snickers',
-    price: 150,
-    quantity: 1,
-    id: 1,
-  }]}
-  dataSource: Array<CartItem> = []
+export class CartComponent implements OnInit {
+  cart: Cart = {
+    items: [
+      {
+        id: 1,
+
+        product: 'https://via.placeholder.com/150',
+        name: 'snickers',
+        price: 150,
+        quantity: 1,
+      },
+    ],
+  };
+
+  dataSource: CartItem[] = [];
   displayedColumns: Array<string> = [
     'product',
     'name',
-    'price',
     'quantity',
     'total',
-    'action'
-  ]
-  ngOnInit(): void  {
-    this.dataSource = this.cart.items
+    'action',
+  ];
+  ngOnInit(): void {
+    this.dataSource = this.cart.items;
   }
-  getTotal(items: Array<CartItem>):number {
-    return  items.map((item) => item.price * item.quantity)
-      .reduce((prev, current)=> prev + current, 0)
+
+  getTotal(items: Array<CartItem>): number {
+    return items
+      .map((item) => item.price * item.quantity)
+      .reduce((prev, current) => prev + current, 0);
   }
 }
